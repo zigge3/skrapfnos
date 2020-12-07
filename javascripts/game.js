3806307a58c74625595f1e9c601dec2834db93ec
Timmy Ylander <timmy.ylander@svenskaspel.se>
2020-12-07 11:29:47 +0100

    Adds particles

--- javascripts/game.js
+++ javascripts/game.js
@@ -1,56 +1,87 @@
-import {Engine, Render, World, Bodies} from 'matter-js'
+import { Engine, Render, World, Bodies } from "matter-js";
 export function Start(container) {
-    const {innerHeight, innerWidth} = window;
-    const engine = Engine.create({
-        options: {
-            width: innerWidth,
-            height: innerHeight
-        }
+  const { innerHeight, innerWidth } = window;
+  const engine = Engine.create({
+    options: {
+      width: innerWidth,
+      height: innerHeight,
+    },
+  });
+
+  // create a renderer
+  const render = Render.create({
+    element: container,
+    engine: engine,
+    options: {
+      width: innerWidth,
+      height: innerHeight,
+    },
+  });
+
+  // create two boxes and a ground
+  const boxA = Bodies.rectangle(innerWidth / 2, innerHeight / 2, 20, 20);
+  const boxB = Bodies.rectangle(innerWidth / 2, innerHeight / 2, 20, 20);
+
+  const wall1 = Bodies.rectangle(
+    innerWidth / 2,
+    innerHeight + 25,
+    innerWidth,
+    50,
+    { isStatic: true }
+  );
+  const wall2 = Bodies.rectangle(-25, innerHeight / 2, 50, innerHeight, {
+    isStatic: true,
+  });
+  const wall3 = Bodies.rectangle(innerWidth / 2, -25, innerWidth, 50, {
+    isStatic: true,
+  });
+  const wall4 = Bodies.rectangle(
+    innerWidth + 25,
+    innerHeight / 2,
+    50,
+    innerHeight,
+    { isStatic: true }
+  );
+  // add all of the bodies to the world
+  World.add(engine.world, [boxA, boxB, wall1, wall2, wall3, wall4]);
+
+  // run the engine
+  Engine.run(engine);
+
+  // run the renderer
+  Render.run(render);
+  window.addEventListener("deviceorientation", (event) => {
+    const x = event.beta;
+    const y = event.gamma;
+    if (x > 90) {
+      x = 90;
+    }
+    if (x < -90) {
+      x = -90;
+    }
+    engine.world.gravity.x = (y / 90) * 2;
+    engine.world.gravity.y = (x / 90) * 2;
+  });
+
+  container.addEventListener("click", (e) => {
+    spawnFnos({ x: e.clientX, y: e.clientY, color: "white" });
+  });
+  function spawnFnos({ color, x, y }) {
+    console.log(x, y);
+    const fnos = Bodies.rectangle(x, y, 2, 2, {
+      color: color,
     });
-
-    // create a renderer
-    const render = Render.create({
-        element: container,
-        engine: engine,
-        options: {
-            width: innerWidth,
-            height: innerHeight
-        }
-    });
-
-    // create two boxes and a ground
-    const boxA = Bodies.rectangle(innerWidth / 2, innerHeight / 2, 20, 20);
-    const boxB = Bodies.rectangle(innerWidth / 2, innerHeight / 2, 20, 20);
-    const wall1 = Bodies.rectangle(innerWidth / 2, innerHeight + 25, innerWidth, 50, { isStatic: true });
-    const wall2 = Bodies.rectangle(-25, (innerHeight / 2), 50, innerHeight, { isStatic: true });
-    const wall3 = Bodies.rectangle(innerWidth / 2, -25, innerWidth, 50, { isStatic: true });
-    const wall4 = Bodies.rectangle(innerWidth + 25, (innerHeight / 2), 50, innerHeight, { isStatic: true });
-    // add all of the bodies to the world
-    World.add(engine.world, [boxA, boxB, wall1, wall2, wall3, wall4]);
-
-    // run the engine
-    Engine.run(engine);
-
-    // run the renderer
-    Render.run(render);
-    window.addEventListener('deviceorientation', event => {
-        const x = event.beta;
-        const y = event.gamma;
-        if (x >  90) { x =  90};
-        if (x < -90) { x = -90};
-        engine.world.gravity.x = y / 90 * 9.8;
-        engine.world.gravity.y = x / 90 * 9.8; 
-    });
-    return {render, engine}
+    World.add(engine.world, [fnos]);
+  }
+  return { render, engine };
 }
 
-export function Stop({engine, render}) {
-    engine.world && World.clear(engine.world);
-    Engine.clear(engine);
-    Render.stop(render);
-    render.canvas.remove();
-    render.canvas = null;
-    render.context = null;
-    render.textures = {};
+export function Stop({ engine, render }) {
+  engine.world && World.clear(engine.world);
+  Engine.clear(engine);
+  Render.stop(render);
+  render.canvas.remove();
+  render.canvas = null;
+  render.context = null;
+  render.textures = {};
 }
-
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 import Head from "next/head";
 import { Start, Stop } from "../javascripts/game.js";
+import { StartKonva } from "../javascripts/konva.js";
 import styles from "../styles/Home.module.css";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, Fragment } from "react";
 export default function Home() {
   const gameContainer = useRef("");
+  const konvaContainer = useRef("");
   const [appStarted, setAppStarted] = useState(false);
   useEffect(() => {
     if (appStarted) {
       const objs = Start(gameContainer.current);
+      StartKonva(konvaContainer.current);
       return () => {
         Stop(objs);
       };
@@ -16,7 +19,10 @@ export default function Home() {
   return (
     <div className={styles.main}>
       {appStarted && (
-        <div className={styles.container} ref={gameContainer}></div>
+        <Fragment>
+          <div className={styles.container} ref={konvaContainer}></div>
+          <div className={styles.container} ref={gameContainer}></div>
+        </Fragment>
       )}
       {!appStarted && (
         <div className={styles.container}>
@@ -33,6 +39,8 @@ export default function Home() {
                     }
                   }
                 );
+              } else {
+                setAppStarted(true);
               }
             }}
           >
